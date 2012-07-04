22722c595ae55906c9732c5a5de294d4d2ddbfdd
Nils Langner <contact@phmlabs.com>
2012-07-04 13:39:55 +0200

    Added filter duplicate flag to JMMD bade class

--- Phm/Jmmd/Jmmd.php
+++ Phm/Jmmd/Jmmd.php
@@ -1,35 +1,39 @@
 <?php
-
 namespace Phm\Jmmd;
-
 use Phm\Jmmd\Rule\Rule;
 use Phm\Jmmd\JMeter\JMeterReport;
 
 class Jmmd
 {
-  private $rules = array();
 
-  public function detect(JMeterReport $report)
-  {
-  	$violations = array();
+    private $rules = array();
 
-    $httpSampleElements = $report->getHttpSampleElements();
-    foreach ($httpSampleElements as $httpSampleElement)
+    public function detect (JMeterReport $report, $filterDuplicates = true)
     {
-      foreach ($this->rules as $rule)
-      {
-        $result = $rule->detect($httpSampleElement);
-        if( !$result->isSuccessful()) {
-        	$violations[$httpSampleElement->getUrl()][] = $result;
+        $violations = array();
+
+        $httpSampleElements = $report->getHttpSampleElements();
+        foreach ($httpSampleElements as $httpSampleElement) {
+            foreach ($this->rules as $rule) {
+                if (! $filterDuplicates ||
+                         ! array_key_exists($httpSampleElement->getUrl(),
+                                $violations) || ! array_key_exists(
+                                get_class($rule),
+                                $violations[$httpSampleElement->getUrl()])) {
+                    $result = $rule->detect($httpSampleElement);
+                    if (! $result->isSuccessful()) {
+                        $violations[$httpSampleElement->getUrl()][get_class(
+                                $rule)][] = $result;
+                    }
+                }
+            }
         }
-      }
+
+        return $violations;
     }
 
-    return $violations;
-  }
-
-  public function addRule(Rule $rule)
-  {
-    $this->rules[] = $rule;
-  }
+    public function addRule (Rule $rule)
+    {
+        $this->rules[] = $rule;
+    }
 }
--- Phm/Jmmd/Report/CsvFormat.php
+++ Phm/Jmmd/Report/CsvFormat.php
@@ -1,20 +1,20 @@
 <?php
-
 namespace Phm\Jmmd\Report;
 
 class CsvFormat
 {
-  public function createReport(array $results)
-  {
-    $resultMessage = "";
 
-    foreach ($results as $url => $resultForUrl)
+    public function createReport (array $results)
     {
-      foreach ($resultForUrl as $result)
-      {
-        $resultMessage .= $url . ";" . $result->getMessage() . "\n";
-      }
+        $resultMessage = "";
+
+        foreach ($results as $url => $rulesWithViolations) {
+            foreach ($rulesWithViolations as $rule => $violations) {
+                foreach ($violations as $violation) {
+                    $resultMessage .= '"'.$url . '";' . $rule . ";" . $violation->getMessage() . "\n";
+                }
+            }
+        }
+        return $resultMessage;
     }
-    return $resultMessage;
-  }
 }
